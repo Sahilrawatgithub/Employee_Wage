61af14113d4d5ee90d7e8073b2a14ddca6c9f49b
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 13:07:20 +0530

    UC1

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-           
+            //UC1 Check Employee is present or absent
+            Random random = new Random();
+            int CheckAttendance = random.Next(0, 2);
+
+            if (CheckAttendance > 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
