9615d6a49e7efdff1208d825897a8a9a66420269
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 21:55:51 +0530

    UC7 compute not print

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        public static void ComputeWage()
+        public static int ComputeWage()
         {
 
             const int part_time = 1;
@@ -28,8 +28,9 @@
                     hours_worked = 0;
                     break;
             }
-            int wage = rate_per_hour * hours_worked;
-            Console.WriteLine($"The wage is {wage}");
+             int wage = rate_per_hour * hours_worked;
+            return wage;
+            //Console.WriteLine($"The wage is {wage}");
         }
 
         
