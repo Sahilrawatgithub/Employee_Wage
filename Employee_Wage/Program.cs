14603eae36260b85e736f20ab15f7df4376ff907
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 20:01:57 +0530

    correction commit

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -1,21 +1,11 @@
-﻿namespace Employee_Wage
+﻿
+namespace Employee_Wage
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //UC1 Check Employee is present or absent
-            Random random = new Random();
-            int CheckAttendance = random.Next(0, 2);
-
-            if (CheckAttendance > 0)
-            {
-                Console.WriteLine("Employee is present");
-            }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
+           
         }
     }
 }
