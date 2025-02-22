8a4b030e75c183f561ff5b600c1742ae2895b445
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 13:14:24 +0530

    UC3

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-           
+            //UC3 Add Part time employee and wage
+            int PartTime = 1;
+            int FullTime = 2;
+
+
+            int Hourly_Rate = 10;
+            int wage = 0;
+
+            Random rand = new Random();
+
+            int check = rand.Next(3);
+
+            if (check == PartTime)
+            {
+                wage = 4 * Hourly_Rate;
+                Console.WriteLine($"Employee works part time and their wage is ${wage}");
+            }
+            else if (check == FullTime)
+            {
+                wage = 8 * Hourly_Rate;
+                Console.WriteLine($"Employee works full time and their wage is ${wage}");
+            }
+            else
+            {
+                Console.WriteLine($"Person did not work. Wage = ${wage}");
+            }
         }
     }
 }
