b0c3eaca1869564b1924b9938ec9009091e1302e
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 21:53:50 +0530

    UC7

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        public static void ComputeWage()
+        {
+
+            const int part_time = 1;
+            const int full_time = 2;
+            int hours_worked = 0;
+            int rate_per_hour = 10;
+
+            Random rand = new Random();
+
+            int check = rand.Next(0, 3);
+
+            switch (check)
+            {
+                case part_time:
+                    hours_worked = 4;
+                    break;
+
+                case full_time:
+                    hours_worked = 8;
+                    break;
+
+                default:
+                    hours_worked = 0;
+                    break;
+            }
+            int wage = rate_per_hour * hours_worked;
+            Console.WriteLine($"The wage is {wage}");
+        }
+
+        
         static void Main(string[] args)
         {
-           
+            //UC7 Compute Employee wage using class methods
+            ComputeWage();
+
         }
     }
 }
