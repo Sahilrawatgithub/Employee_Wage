9821192cb970d7a335d4973683777d98e3904374
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 20:05:48 +0530

    correctNow

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -5,7 +5,18 @@ namespace Employee_Wage
     {
         static void Main(string[] args)
         {
-           
+            //UC1 Check Employee is present or absent
+            Random random = new Random();
+            int CheckAttendance = random.Next(0, 2);
+
+            if (CheckAttendance > 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
