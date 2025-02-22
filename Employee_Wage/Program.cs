b5e3908dd445dfbe9e780497df3b8ec34dbf6917
Sahilrawatgithub <sahilrawat12741@gmail.com>
2025-02-22 13:09:48 +0530

    UC2

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-           
+            //UC2: Calculate daily employee wage
+            int isFullTime = 1;
+            int ratePerHour = 10;
+
+            int hoursWorked = 0;
+            Random rand = new Random();
+            int check = rand.Next(0, 2);
+
+            if (check == isFullTime)
+            {
+                hoursWorked = 8;
+            }
+            else
+            {
+                hoursWorked = 0;
+            }
+
+            int Wage = hoursWorked * ratePerHour;
+
+            Console.WriteLine($"Daily wage is ${Wage}");
+
         }
     }
 }
